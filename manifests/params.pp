54eb8db146bcbfc4c2e67674dc3b018afe26dced
Romain Tartière <romain@blogreen.org>
2016-11-22 19:32:59 +0100

    Fix some warnings

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,17 @@
 class sogo3::params {
   case $::osfamily {
-    'Debian': {
+    'debian': {
       $package = ['sogo', 'sogo-common']
       $service = 'sogo'
       $sogo_conf = '/etc/sogo/sogo.conf'
-     }
-    'FreeBSD': {
+    }
+    'freebsd': {
       $package = 'sogo3'
       $service = 'sogod'
       $sogo_conf = '/usr/local/etc/sogo3/sogo.conf'
+    }
+    default: {
+      fail("Unsupported osfamily: ${::osfamily}")
     }
   }
 }
